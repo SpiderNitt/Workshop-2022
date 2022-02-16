86ee6999791b8c030bfddbef304611bc6c92f651
Bhuvana Chandra P <bhuvanachandraprabhu@gmail.com>
2022-02-16 15:26:32 +0530

    Added localstorage

--- script.js
+++ script.js
@@ -1,20 +1,112 @@
 let position =1;
 let word_length=0;
 let ans_array =[];
-let tile;
+let tile='';
 let answer_word = "right";
 let position_checker ;
 let letter = document.querySelectorAll('.key');
 let letterType ;
-function checkAnswer()
+let corrects=0;
+let won = false;
+let boardState =[];
+let StateLocalStorage =[];
+let attempts =0;
+// localStorage.clear();
+function fillBoard()
 {
-  console.log(ans_array);
-  tile =1;
-  
   position_checker = position-5;
- 
   for(let i =0;i<5;i++)
   {
+    tile += ans_array[i];
+    letter.forEach(function(element){ 
+      if(element.dataset.key == ans_array[i] )
+      {
+        letterType  = element;
+        
+      }
+    })
+    if(ans_array[i] == answer_word[i])
+    {
+      push_letter = document.getElementById(`${position_checker}`);
+      push_letter.innerHTML = ans_array[i];
+      push_letter.style.backgroundColor = "green";
+      letterType.style.backgroundColor = "green";
+      push_letter.style.color = "white";
+      letterType.style.color = "white";
+      corrects++;
+    }
+    else if(answer_word.includes(ans_array[i]))
+    {
+      push_letter = document.getElementById(`${position_checker}`);
+      push_letter.innerHTML = ans_array[i];
+      push_letter.style.backgroundColor = "yellow";
+      letterType.style.backgroundColor = "yellow";
+      push_letter.style.color = "white";
+      letterType.style.color = "white";
+    }
+    else{
+      push_letter = document.getElementById(`${position_checker}`);
+      push_letter.innerHTML = ans_array[i];
+      push_letter.style.backgroundColor = "grey";
+      push_letter.style.color = "white";
+      letterType.style.color = "white";
+      letterType.style.backgroundColor = "grey";
+      
+    }
+    position_checker++;
+  }
+  tile ='';
+  if(corrects == 5)
+  {
+    won = true;
+    console.log("game over");
+  }
+  corrects =0;
+}
+function settingLocalStograge()
+{
+  localStorage.setItem("answer",answer_word);
+  if(localStorage.getItem("attempts") == null || localStorage.getItem("attempts") == undefined)
+    localStorage.setItem("attempts",0);
+  StateLocalStorage = JSON.parse(localStorage.getItem('boardState'));
+ 
+  
+  position=1;
+  if(StateLocalStorage != null && StateLocalStorage != undefined){
+    console.log(StateLocalStorage);
+    console.log(StateLocalStorage.length);
+    boardState = StateLocalStorage;
+    attempts = JSON.parse(localStorage.getItem('attempts'));
+    for(let i=0;i<StateLocalStorage.length;i++)
+    {
+      for(let j=0;j<5;j++)
+      {
+        ans_array[j] = StateLocalStorage[i][j];
+      }
+      position = position+5;
+      fillBoard();
+    }
+  }
+
+}
+settingLocalStograge();
+function updateLocalStorage()
+{
+  // console.log(boardState);
+  localStorage.setItem("boardState",JSON.stringify(boardState));
+  // StateLocalStorage = JSON.parse(localStorage.getItem('boardState'));
+  console.log(attempts);
+  localStorage.setItem("attempts",attempts);
+  // console.log(StateLocalStorage);
+  // console.log(boardState);
+}
+function checkAnswer()
+{
+  //console.log(ans_array);
+  position_checker = position-5;
+  for(let i =0;i<5;i++)
+  {
+    tile += ans_array[i];
     letter.forEach(function(element){ 
       if(element.dataset.key == ans_array[i] )
       {
@@ -29,6 +121,7 @@ function checkAnswer()
       letterType.style.backgroundColor = "green";
       push_letter.style.color = "white";
       letterType.style.color = "white";
+      corrects++;
     }
     else if(answer_word.includes(ans_array[i]))
     {
@@ -48,9 +141,29 @@ function checkAnswer()
     }
     position_checker++;
   }
+  // console.log(tile);
+  // localStorage.setItem("attempts",attempts);
+  attempts = JSON.parse(localStorage.getItem("attempts"));
+  // console.log(attempts);
+  // console.log(boardState);
+  console.log(boardState);
+  boardState[attempts] = tile;
+  console.log(boardState);
+  StateLocalStorage = boardState;
+  attempts++;
+  updateLocalStorage();
+  tile ='';
+  
+  if(corrects == 5)
+  {
+    won = true;
+    console.log("game over");
+  }
+  corrects =0;
  
 
 }
+
 window.addEventListener('keydown', function(e){
   let l = e.key;
   if(l == "Enter")
@@ -70,19 +183,19 @@ window.addEventListener('keydown', function(e){
   }
   else if(l == "Backspace")
   {
-    console.log(position);
+   // console.log(position);
     if(word_length){
       position--;
       push_letter = document.getElementById(`${position}`);
-      console.log(push_letter);
+     // console.log(push_letter);
       push_letter.innerHTML = "";
       word_length--;
     }
     
   }
   else{
-    if(word_length != 5){
-      console.log(l);
+    if(word_length != 5 && !won){
+     // console.log(l);
       push_letter = document.getElementById(`${position}`);
       push_letter.innerHTML = l;
       position++;
@@ -113,15 +226,15 @@ letter.forEach(function(element){
       if(word_length){
         position--;
         push_letter = document.getElementById(`${position}`);
-        console.log(push_letter);
+        //console.log(push_letter);
         push_letter.innerHTML = "";
         word_length--;
       }
       
     }
     else{
-      if(word_length != 5){
-        console.log(l);
+      if(word_length != 5 && !won){
+        //console.log(l);
         push_letter = document.getElementById(`${position}`);
         push_letter.innerHTML = l;
         position++;
